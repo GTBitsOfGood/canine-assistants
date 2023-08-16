c2b35b3e14bc460146bc2bbf274fed1bb75d3337
afazio1 <alexa.fazio04@gmail.com>
2023-08-15 22:15:29 -0400

    chore: cleanup default index & styles

--- src/pages/api/hello.js
+++
@@ -1,6 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-export default function handler(req, res) {
-  console.log("hello");
-  res.status(200).json({ name: "test lint" });
-}
