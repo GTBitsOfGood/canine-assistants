9e54ad71314587e0f3e452179d60595fc1ee9fd1
jpise3 <63403110+jaipise@users.noreply.github.com>
2023-10-02 13:52:34 -0400

    Fixed the case where object id is valid but does not exist

--- src/pages/api/logs/[id].js
+++ src/pages/api/logs/[id].js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
       const { id } = req.query;
       const data = await getLogById(id);
 
-      if (data.length === 0) {
+      if (data === null || data.length === 0) {
         res.status(404).json({
           success: false,
           error: "Unable to retrieve log because it doesn't exist",
