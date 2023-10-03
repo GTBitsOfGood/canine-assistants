3f5111df2ec0ae1cd241789985120eefeff05868
afazio1 <alexa.fazio04@gmail.com>
2023-10-03 13:26:33 -0400

    fix enum chip colors

--- src/components/DogTable.jsx
+++ src/components/DogTable.jsx
@@ -13,7 +13,6 @@ import {
 import { Chip, ChipTypeStyles } from "./Chip";
 import SearchTagDisplay from "./SearchTagDisplay";
 import dateutils from "@/utils/dateutils";
-import { consts } from "@/utils/consts";
 
 /**
  *
@@ -63,9 +62,9 @@ export default function DogTable() {
 
   // TEMPORARY until enum situation is figured out
   const chipTypeMapping = {
-    [consts.concernArray[0]]: ChipTypeStyles.NoConcern,
-    [consts.concernArray[1]]: ChipTypeStyles.SomeConcern,
-    [consts.concernArray[2]]: ChipTypeStyles.HighConcern,
+    ["No Concern"]: ChipTypeStyles.NoConcern,
+    ["Some Concern"]: ChipTypeStyles.SomeConcern,
+    ["High Concern"]: ChipTypeStyles.HighConcern,
   };
 
   /**
