b7b1210f0e2eee6cb8630bd848f01ab31733478b
jpise3 <63403110+jaipise@users.noreply.github.com>
2023-10-11 16:30:33 -0400

    Completed functionality to make api calls to get users

--- src/pages/api/users/index.js
+++ src/pages/api/users/index.js
@@ -1,15 +1,39 @@
-import { createUser } from "../../../../server/db/actions/User";
+import { createUser, getUsers } from "../../../../server/db/actions/User";
 
 export default async function handler(req, res) {
-  const { username, hash } = req.query;
+  if (req.method == "POST") {
+    const { username, hash } = req.query;
 
-  let userId;
-  try {
-    userId = await createUser(username, hash);
-  } catch (e) {
-    res.status(500).json({ error: "Unable to create user" });
-    return;
+    let userId;
+    try {
+      userId = await createUser(username, hash);
+    } catch (e) {
+      res.status(500).json({ error: "Unable to create user" });
+      return;
+    }
+
+    res.status(200).json({ id: userId });
   }
 
-  res.status(200).json({ id: userId });
+  if (req.method == "GET") {
+    try {
+      const users = await getUsers();
+
+      return res.status(200).json({
+        success: true,
+        message: "Successfully retrieved users",
+        data: users,
+      });
+    } catch (e) {
+      return res.status(500).json({
+        success: false,
+        message: e.message,
+      });
+    }
+  }
+
+  return res.status(405).json({
+    success: false,
+    message: `Request method ${req.method} is not allowed`,
+  });
 }
