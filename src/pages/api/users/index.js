634150dd04df073b33c18010a4eb4172b74ed5de
jpise3 <63403110+jaipise@users.noreply.github.com>
2023-10-11 17:06:25 -0400

    updated user schema based on auth generated properties

--- src/pages/api/users/index.js
+++ src/pages/api/users/index.js
@@ -4,7 +4,12 @@ import { consts } from "@/utils/consts";
 
 const userSchema = z.object({
   username: z.string(),
-  hash: z.enum(consts.topicArray),
+  hash: z.string(),
+  name: z.string(),
+  email: z.string(),
+  image: z.string().optional(),
+  emailVerified: z.boolean().default(null),
+  role: z.array(z.enum(consts.roleArray)).optional(),
 });
 
 export default async function handler(req, res) {
@@ -20,4 +25,7 @@ export default async function handler(req, res) {
   }
 
   res.status(200).json({ id: userId });
+
+  if (req.method == "PATCH") {
+  }
 }
