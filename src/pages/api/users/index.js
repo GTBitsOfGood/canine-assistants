0fa04271dae15363abb07cbcc4663cb16f1caa6b
jpise3 <63403110+jaipise@users.noreply.github.com>
2023-10-11 16:49:49 -0400

    inserted a return statement for res status

--- src/pages/api/users/index.js
+++ src/pages/api/users/index.js
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
       return;
     }
 
-    res.status(200).json({ id: userId });
+    return res.status(200).json({ id: userId });
   }
 
   if (req.method == "GET") {
