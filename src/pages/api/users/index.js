881098a5c7ebd6555d40d0cec0bf6b08482b9310
jpise3 <63403110+jaipise@users.noreply.github.com>
2023-10-11 16:48:36 -0400

    created a preliminary user schema

--- src/pages/api/users/index.js
+++ src/pages/api/users/index.js
@@ -1,4 +1,11 @@
 import { createUser } from "../../../../server/db/actions/User";
+import { z } from "zod";
+import { consts } from "@/utils/consts";
+
+const userSchema = z.object({
+  username: z.string(),
+  hash: z.enum(consts.topicArray),
+});
 
 export default async function handler(req, res) {
   const { username, hash } = req.query;
