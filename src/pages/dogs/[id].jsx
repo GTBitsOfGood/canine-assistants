61def0010abef98e33363636ff13452c267275c7
tawsifkamal <tawsifkamal123@gmail.com>
2023-10-13 08:03:44 -0400

    barebores

--- src/pages/dogs/[id].jsx
+++ src/pages/dogs/[id].jsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import dateutils from "@/utils/dateutils";
+import { useForm } from "react-hook-form";
 import {
   ChevronLeftIcon,
   PencilSquareIcon,
@@ -82,106 +83,113 @@ export default function IndividualDogPage() {
         </Link>
       </div>
 
-      <div className="flex gap-8">
-        {dog.image ? (
-          <Image alt="Dog" width={300} height={300} src={dog.image} />
-        ) : (
-          <>
-            <div
-              className={
-                "w-[300px] h-[300px] bg-primary-gray flex items-center justify-center rounded-lg"
-              }
-            >
-              <Image priority src={dogplaceholdericon} alt="Dog Placeholder" />
-            </div>
-            <div className="flex-col">
-              <div className="flex justify-between">
-                <div className="flex gap-4">
-                  <Chip
-                    label={dog.location}
-                    type={ChipTypeStyles[dog.location] || ChipTypeStyles.Tag}
-                  />
-                  <div className="flex justify-center items-center space-x-2">
-                    <Image
-                      priority
-                      src={dog.gender === "Male" ? maleicon : femaleicon}
-                      alt="Male Dog"
+      <form>
+        <div className="flex gap-8">
+          {dog.image ? (
+            <Image alt="Dog" width={300} height={300} src={dog.image} />
+          ) : (
+            <>
+              <div
+                className={
+                  "w-[300px] h-[300px] bg-primary-gray flex items-center justify-center rounded-lg"
+                }
+              >
+                <Image
+                  priority
+                  src={dogplaceholdericon}
+                  alt="Dog Placeholder"
+                />
+              </div>
+              <div className="flex-col">
+                <div className="flex justify-between">
+                  <div className="flex gap-4">
+                    <Chip
+                      label={dog.location}
+                      type={ChipTypeStyles[dog.location] || ChipTypeStyles.Tag}
                     />
+                    <div className="flex justify-center items-center space-x-2">
+                      <Image
+                        priority
+                        src={dog.gender === "Male" ? maleicon : femaleicon}
+                        alt="Male Dog"
+                      />
 
-                    <div>Male</div>
-                  </div>
-                </div>
-              </div>
-
-              <div className="pt-6 pl-1 font-bold text-3xl">{dog.name}</div>
-              <div className="flex space-x-16">
-                <div className="flex-col pt-8 pl-1 text-lg space-y-2">
-                  <div>
-                    Birth Date:{" "}
-                    {dateutils.getDateString(new Date(dog.dateOfBirth))}
-                  </div>
-                  <div>Sex: {dog.gender}</div>
-
-                  <div>Breed: {dog.breed}</div>
-                  <div>Coat Color: N/A</div>
-                </div>
-
-                <div className="flex-col pt-8 pl-1 text-lg space-y-2">
-                  {dog.location === "Placed" ? (
-                    <>
-                      <div>Placement: N/A</div>
-                      <div>Partner: N/A</div>
-                      <div>Placement Camp: N/A</div>
-                    </>
-                  ) : (
-                    <>
-                      <div>Housing: N/A</div>
-                      <div>Instructor: N/A</div>
-                    </>
-                  )}
-                </div>
-              </div>
-            </div>
-            <div className="grow flex gap-4 justify-end">
-              <div className="flex justify-center space-x-2">
-                <PencilSquareIcon className="h-5" />
-
-                <div>Edit</div>
-              </div>
-              <div className="flex justify-center space-x-2">
-                <TrashIcon className="h-5" />
-
-                <div>Delete</div>
-              </div>
-            </div>
-          </>
-        )}
-      </div>
-
-      <div className="mt-8 shadow-xl rounded-lg text-md w-full text-left relative overflow-hidden bg-foreground p-8">
-        <TabSection defaultTab="information">
-          <div label="information">
-            <div className="w-2/3 grid grid-cols-3 gap-16">
-              {Object.keys(dogInformationSchema).map((category) => (
-                <div className="col" key={category}>
-                  <div className="flex-col space-y-4 text-lg">
-                    <div className="text-xl">
-                      <strong>{category}</strong>
+                      <div>Male</div>
                     </div>
-
-                    {Object.keys(dogInformationSchema[category]).map((col) => (
-                      <div key={col}>
-                        {col}: {dogInformationSchema[category][col]}
-                      </div>
-                    ))}
                   </div>
                 </div>
-              ))}
+
+                <div className="pt-6 pl-1 font-bold text-3xl">{dog.name}</div>
+                <div className="flex space-x-16">
+                  <div className="flex-col pt-8 pl-1 text-lg space-y-2">
+                    <div>
+                      Birth Date:{" "}
+                      {dateutils.getDateString(new Date(dog.dateOfBirth))}
+                    </div>
+                    <div>Sex: {dog.gender}</div>
+
+                    <div>Breed: {dog.breed}</div>
+                    <div>Coat Color: N/A</div>
+                  </div>
+
+                  <div className="flex-col pt-8 pl-1 text-lg space-y-2">
+                    {dog.location === "Placed" ? (
+                      <>
+                        <div>Placement: N/A</div>
+                        <div>Partner: N/A</div>
+                        <div>Placement Camp: N/A</div>
+                      </>
+                    ) : (
+                      <>
+                        <div>Housing: N/A</div>
+                        <div>Instructor: N/A</div>
+                      </>
+                    )}
+                  </div>
+                </div>
+              </div>
+              <div className="grow flex gap-4 justify-end">
+                <button className="flex justify-center space-x-2 h-min">
+                  <PencilSquareIcon className="h-5" />
+                  Edit
+                </button>
+                <div className="flex justify-center space-x-2">
+                  <TrashIcon className="h-5" />
+
+                  <div>Delete</div>
+                </div>
+              </div>
+            </>
+          )}
+        </div>
+
+        <div className="mt-8 shadow-xl rounded-lg text-md w-full text-left relative overflow-hidden bg-foreground p-8">
+          <TabSection defaultTab="information">
+            <div label="information">
+              <div className="w-2/3 grid grid-cols-3 gap-16">
+                {Object.keys(dogInformationSchema).map((category) => (
+                  <div className="col" key={category}>
+                    <div className="flex-col space-y-4 text-lg">
+                      <div className="text-xl">
+                        <strong>{category}</strong>
+                      </div>
+
+                      {Object.keys(dogInformationSchema[category]).map(
+                        (col) => (
+                          <div key={col}>
+                            {col}: {dogInformationSchema[category][col]}
+                          </div>
+                        )
+                      )}
+                    </div>
+                  </div>
+                ))}
+              </div>
             </div>
-          </div>
-          <div label="logs">logs</div>
-        </TabSection>
-      </div>
+            <div label="logs">logs</div>
+          </TabSection>
+        </div>
+      </form>
     </div>
   );
 }
