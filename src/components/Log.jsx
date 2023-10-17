9cd5534443eee9ddd57a54a2b69a5214ca1b322c
Siri Tanguturi <siri.tanguturi1@gmail.com>
2023-10-16 21:56:59 -0400

    adding description truncator + minor styling changes

--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -1,7 +1,8 @@
 import TagDisplay from "@/components/TagDisplay";
+import { useState } from "react";
 
 export default function Log({ log }) {
-  // TODO: show more stuff
+  const [showMore, setShowMore] = useState(false);
   const createdAt = new Date(log.createdAt);
 
   const tags = [
@@ -14,24 +15,42 @@ export default function Log({ log }) {
 
   return (
     <div className="bg-primary-background p-4 my-4">
-      <div className="">
-        <div className="flex justify-between">
+      <div className="flex justify-between">
+        <div className="flex flex-col">
           <h2>{log.title}</h2>
-          <TagDisplay tags={tags} removeTag={null} />
+          <div className="flex flex-row">
+            <p className="text-secondary-text font-regular w-fit">
+              {"Author: " + log.author}
+            </p>
+            <p className="text-secondary-text font-regular mx-5 w-fit">
+              {"Date: " + createdAt.toLocaleDateString()}
+            </p>
+            <p className="text-secondary-text font-regular w-fit">
+              {"Time: " + createdAt.toLocaleTimeString("en-US")}
+            </p>
+          </div>
         </div>
-        <div className="flex flex-row">
-          <p className="text-secondary-text font-regular w-fit">
-            {"Author: " + log.author}
-          </p>
-          <p className="text-secondary-text font-regular mx-5 w-fit">
-            {"Date: " + createdAt.toLocaleDateString()}
-          </p>
-          <p className="text-secondary-text font-regular w-fit">
-            {"Time: " + createdAt.toLocaleTimeString("en-US")}
-          </p>
-        </div>
+        <TagDisplay tags={tags} removeTag={null} />
       </div>
-      <p className="pt-4">{log.description}</p>
+      {log.description.length > 250 ? (
+        <div>
+          <p className="pt-4">
+            {showMore
+              ? log.description
+              : log.description.substring(0, 250) + "..."}
+          </p>
+          <div className="flex flex-row-reverse">
+            <button
+              className="font-bold"
+              onClick={() => setShowMore(!showMore)}
+            >
+              {showMore ? "Show Less" : "Show More"}
+            </button>
+          </div>
+        </div>
+      ) : (
+        <p className="pt-4">{log.description}</p>
+      )}
     </div>
   );
 }
