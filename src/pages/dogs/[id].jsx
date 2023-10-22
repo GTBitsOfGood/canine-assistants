412ddef188c349e0a47a0e6d0f46603055c6846f
Siri Reddi Tanguturi <112362569+siri-star@users.noreply.github.com>
2023-10-22 15:15:48 -0400

    bug fix: displaying newly added logs (#68)
    * Fixing bug: newly added logs now are displayed
    
    * reverse chronologically sorting logs

--- src/pages/dogs/[id].jsx
+++ src/pages/dogs/[id].jsx
@@ -26,7 +26,7 @@ import Log from "@/components/Log";
  */
 export default function IndividualDogPage() {
   const [data, setData] = useState();
-  const [ showLogModal, setShowLogModal ] = useState(false);
+  const [showLogModal, setShowLogModal] = useState(false);
 
   const router = useRouter();
 
@@ -35,10 +35,10 @@ export default function IndividualDogPage() {
   const [logs, setLogs] = useState([]);
   const [filteredLogs, setFilteredLogs] = useState([]);
 
-  useEffect(() => {
-    let search = {};
-    search.dog = router.query.id;
+  let search = {};
+  search.dog = router.query.id;
 
+  useEffect(() => {
     if (router.query.id) {
       fetch(`/api/dogs/${router.query.id}`)
         .then((res) => res.json())
@@ -52,16 +52,19 @@ export default function IndividualDogPage() {
       })
         .catch((err) => setLogs([]))
         .then((res) => res.json())
-        .then((data) => setLogs(data));
+        .then((data) =>
+          setLogs(
+            !data || data === undefined || !data.success
+              ? []
+              : data.data.reverse()
+          )
+        );
     }
   }, [router.query]);
 
-  // get all logs
-  const allLogs = !logs || logs === undefined || !logs.success ? [] : logs.data;
-
   useEffect(() => {
     // filter logs by search query
-    const searchQueryFilteredLogs = allLogs.filter(
+    const searchQueryFilteredLogs = logs.filter(
       (log) =>
         log.title.toLowerCase().includes(searchQuery) ||
         log.description.toLowerCase().includes(searchQuery)
@@ -170,19 +173,39 @@ export default function IndividualDogPage() {
       {showLogModal ? (
         <>
           <LogModal
-            dogId={ dog._id }
-            userId={ dog.instructors[0]._id }
+            dogId={dog._id}
+            userId={dog.instructors[0]._id}
             onClose={() => {
               setShowLogModal(false);
             }}
             onSubmit={(success) => {
               // TODO toast animation
               if (success) {
+                // update logs to display
+                fetch("/api/logs/search", {
+                  method: "POST",
+                  headers: {
+                    "Content-Type": "application/json",
+                  },
+                  body: JSON.stringify(search),
+                })
+                  .catch((err) => setLogs([]))
+                  .then((res) => res.json())
+                  .then((data) =>
+                    setLogs(
+                      !data || data === undefined || !data.success
+                        ? []
+                        : data.data.reverse()
+                    )
+                  );
+
                 toast.custom((t) => (
-                  <div className={`h-12 px-6 py-4 rounded shadow justify-center items-center inline-flex bg-ca-green text-white text-lg font-normal
-                    ${t.visible ? 'animate-enter' : 'animate-leave'}`}
+                  <div
+                    className={`h-12 px-6 py-4 rounded shadow justify-center items-center inline-flex bg-ca-green text-white text-lg font-normal
+                    ${t.visible ? "animate-enter" : "animate-leave"}`}
                   >
-                    <span className="font-bold">New log</span>&nbsp;<span>was successfully added.</span>
+                    <span className="font-bold">New log</span>&nbsp;
+                    <span>was successfully added.</span>
                   </div>
                 ));
               } else {
@@ -195,7 +218,7 @@ export default function IndividualDogPage() {
             }}
           />
         </>
-      ) : null }
+      ) : null}
       <div className="py-6 flex items-center">
         <ChevronLeftIcon className="w-4 mr-2" />
         <Link href="/dogs" className="text-lg text-secondary-text">
@@ -306,9 +329,13 @@ export default function IndividualDogPage() {
                 className=" px-4 py-2.5 bg-ca-pink rounded border border-ca-pink-shade justify-start items-center gap-2 flex"
                 onClick={() => setShowLogModal(true)}
               >
-          <div className="text-foreground h-4 w-4 relative">{<PlusIcon />}</div>
-          <div className="text-foreground text-base font-medium">Add a log</div>
-        </button>
+                <div className="text-foreground h-4 w-4 relative">
+                  {<PlusIcon />}
+                </div>
+                <div className="text-foreground text-base font-medium">
+                  Add a log
+                </div>
+              </button>
               <LogSearchFilterBar
                 filters={appliedFilters}
                 setFilters={setAppliedFilters}
@@ -322,8 +349,8 @@ export default function IndividualDogPage() {
                 return <Log log={log} key={log._id} />;
               })}
               <div className="flex justify-center">
-                Displaying {filteredLogs.length} out of {allLogs.length}{" "}
-                {allLogs.length == 1 ? "log" : "logs"}
+                Displaying {filteredLogs.length} out of {logs.length}{" "}
+                {logs.length == 1 ? "log" : "logs"}
               </div>
             </div>
           </div>
