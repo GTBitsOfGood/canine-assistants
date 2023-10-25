987f32d3bd04087417122b7bacee1d60c6f9f93f
Yerti <36551149+UZ9@users.noreply.github.com>
2023-10-24 20:12:29 -0400

    :sparkles: Add authentication

--- src/middleware.js
+++ src/middleware.js
@@ -1,5 +1,3 @@
-// export { default } from "next-auth/middleware"
+export { default } from "next-auth/middleware"
 
-// export const config = { matcher: [ '/((?!login|signup).*)' ] }
-
-export function middleware() {}
+export const config = { matcher: [ '/((?!login|signup).*)' ] }
