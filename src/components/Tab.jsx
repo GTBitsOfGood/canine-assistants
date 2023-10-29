f5c1da883239f7d96ec64894a8ef253f5ea94983
Siri Reddi Tanguturi <112362569+siri-star@users.noreply.github.com>
2023-10-29 13:14:55 -0400

    User Settings Page (#80)
    * Linking pages via navbar & adding active tab styling
    
    * adding account page (TODO: (1) error toast appearing on empty name submit, (2) deactivate modal not updating role [both returning success], (3) navbar, responsive to route)
    
    * updating button functionality: error handling :D
    
    * Navbar updates: responsive to router pathname state
    
    * minor styling fixes
    
    * bug fix: bounding width and truncating long words (#81)
    
    * merge with dev
    
    * small cleanup
    
    * responding to comments
    
    ---------
    
    Co-authored-by: afazio1 <alexa.fazio04@gmail.com>

--- src/components/Tab.jsx
+++ src/components/Tab.jsx
@@ -2,14 +2,21 @@ import stringUtils from "@/utils/stringutils";
 
 /**
  * A modular Tab used in a TabSection
- * 
- * @param {{ activeTab: string, label: string, onTabClick: (e: React.MouseEvent<HTMLButtonElement>) => void}} 
- * @returns 
+ *
+ * @param {{ activeTab: string, label: string, onTabClick: (e: React.MouseEvent<HTMLButtonElement>) => void}}
+ * @returns
  */
 export default function Tab({ activeTab, label, onTabClick }) {
-    return (
-        <li className={`${activeTab.toLowerCase() === label.toLowerCase() ? "border-primary-gray" : ""} text-xl font-bold px-7 sm:px-10 border-b-4 text-primary-text hover:bg-gray-50`} onClick={onTabClick}>
-            <a href="#">{stringUtils.upperFirstLetter(label)}</a>
-        </li>
-    )
+  return (
+    <li
+      className={`${
+        activeTab.toLowerCase() === label.toLowerCase()
+          ? "border-primary-gray"
+          : ""
+      } text-xl font-bold px-7 sm:px-10 border-b-4 text-primary-text hover:bg-gray-100`}
+      onClick={onTabClick}
+    >
+      <a href="#">{stringUtils.upperFirstLetter(label)}</a>
+    </li>
+  );
 }
