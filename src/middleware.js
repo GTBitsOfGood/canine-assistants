4182664813bf74e92591f7ac3b28c8b528880397
Yerti <36551149+UZ9@users.noreply.github.com>
2023-10-29 19:38:11 -0400

    :construction: Mirror [...nextauth] in middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,3 +1,44 @@
-export { default } from "next-auth/middleware"
+import withAuth from "next-auth/middleware";
 
-export const config = { matcher: [ '/((?!login|signup).*)' ] }
+export default withAuth({
+  // Use JWTs for everything session-related
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  pages: {
+    signIn: "/login",
+    // newUser: "/dogs",
+    // error: "/login",
+  },
+  callbacks: {
+    async signIn({ user }) {
+      if (!user.role) {
+        user.role = "Volunteer";
+      }
+
+      return true;
+    },
+    async jwt({ token, user }) {
+      if (user) {
+        token.user = {
+          _id: user._id,
+          email: user.email,
+          name: user.name,
+          role: user.role,
+        };
+
+        return token;
+      }
+    },
+
+    session: async ({ session, token }) => {
+      if (token) {
+        session.user = token.user;
+        session.role = token.role;
+      }
+
+      return session;
+    },
+  },
+});
