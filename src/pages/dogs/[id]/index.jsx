8cec26949cd222b087584797ac9a69e14b674215
emilyliublair <emilyliu.us@gmail.com>
2024-01-30 17:26:39 -0500

    forgot to finish state change for log tab

--- src/pages/dogs/[id]/index.jsx
+++ src/pages/dogs/[id]/index.jsx
@@ -66,7 +66,7 @@ export default function IndividualDogPage() {
       setShowLogTab(false)
     } else {
       setShowFormTab(true)
-      setShowLogTab
+      setShowLogTab(true)
     }
   }, [isEdit])
 
