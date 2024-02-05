b5bf5c5aeac8eed646b7cc19438d0e2c1141514a
Parker Abramson <parkerrabramson@gmail.com>
2024-02-04 19:09:58 -0500

    Add file uploads endpoint and database action

--- server/db/actions/Image.js
+++ server/db/actions/Image.js
@@ -1,3 +1,5 @@
+const crypto = require("crypto");
+
 const bucketId = process.env.BACKBLAZE_BUCKET_ID;
 const applicationKeyId = process.env.BACKBLAZE_KEY_ID;
 const applicationKey = process.env.BACKBLAZE_APP_KEY;
@@ -10,6 +12,79 @@ export async function backblazeConnect() {
         "Basic " +
         Buffer.from(applicationKeyId + ":" + applicationKey).toString("base64"),
     },
+  })
+    .then((response) => response.json())
+    .then((res) => {
+      return {
+        success: true,
+        data: res,
+      };
+    })
+    .catch((err) => {
+      return {
+        success: false,
+        error: err,
+      };
+    });
+}
+
+/**
+ * Uploads an image to the Backblaze database
+ * @param {String} dogId Id of the dog associated with the image
+ * @param {Buffer} image Buffer of the image, use API endpoint for help
+ * @returns JSON object with success status and information about the upload or error message
+ */
+export async function uploadImage(dogId, image) {
+  const connection = await backblazeConnect();
+
+  if (!connection.success) {
+    return connection;
+  }
+
+  const uploadData = await fetch(
+    `${connection.data.apiInfo.storageApi.apiUrl}/b2api/v3/b2_get_upload_url?bucketId=${bucketId}`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: connection.data.authorizationToken,
+      },
+    },
+  )
+    .then((response) => response.json())
+    .then((res) => {
+      return {
+        success: true,
+        data: res,
+      };
+    })
+    .catch((err) => {
+      return {
+        success: false,
+        error: err,
+      };
+    });
+
+  if (!uploadData.success) {
+    return uploadData;
+  }
+
+  const imageName = (
+    typeof dogId === "object" ? dogId.toString() : dogId
+  ).concat(".png");
+
+  return fetch(uploadData.data.uploadUrl, {
+    method: "POST",
+    headers: {
+      Authorization: uploadData.data.authorizationToken,
+      "Content-Type": "image/png",
+      "Content-Length": image.length,
+      "X-Bz-File-Name": imageName,
+      "X-Bz-Content-Sha1": crypto
+        .createHash("sha1")
+        .update(image)
+        .digest("hex"),
+    },
+    body: image,
   })
     .then((response) => response.json())
     .then((res) => {
--- src/pages/api/images/index.js
+++ src/pages/api/images/index.js
@@ -1,4 +1,7 @@
-import { backblazeConnect } from "../../../../server/db/actions/Image";
+import {
+  backblazeConnect,
+  uploadImage,
+} from "../../../../server/db/actions/Image";
 
 export default async function handler(req, res) {
   if (req.method === "GET") {
@@ -16,6 +19,34 @@ export default async function handler(req, res) {
         data: results.error,
       });
     }
+  } else if (req.method === "POST") {
+    // Create file buffer from requst stream
+    const image = await new Promise((resolve) => {
+      const chunks = [];
+
+      req.on("data", (chunk) => {
+        chunks.push(chunk);
+      });
+
+      req.on("end", () => {
+        resolve(Buffer.concat(chunks));
+      });
+    });
+
+    const results = await uploadImage(req.headers["dog-id"], image);
+
+    if (results.success) {
+      return res.status(200).send({
+        success: true,
+        message: "Image successfully uploaded",
+        data: results.data,
+      });
+    } else {
+      return res.status(400).send({
+        success: false,
+        data: results.error,
+      });
+    }
   }
 
   return res.status(405).send({
@@ -23,3 +54,10 @@ export default async function handler(req, res) {
     message: `Request method ${req.method} is not allowed`,
   });
 }
+
+// Request body is not parsed so it is received as a stream
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
