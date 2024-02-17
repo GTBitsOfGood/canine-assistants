d6d49b3f7e2377fc0cc6bc7667e479c904a779c6
Parker Abramson <parkerrabramson@gmail.com>
2024-02-17 17:28:13 -0500

    Add JavaDocs for Toast function

--- src/components/Toast.js
+++ src/components/Toast.js
@@ -5,6 +5,13 @@ const CA_GREEN_SHADE = "#052E16";
 const CA_PINK = "#A70C53";
 const CA_PINK_SHADE = "#730034";
 
+/**
+ * Function to create toasts using React Hot Toast
+ * @param {boolean} success if toast should have success or error styles
+ * @param {string} message message to be shown on the toast without any bolded word
+ * @param {string} bold if message starts with a bolded word/phrase, the phrase should be in this field and not the message field
+ * @returns Toast
+ */
 export function Toast({ success, message, bold }) {
   return toast(
     <span className="text-center">
