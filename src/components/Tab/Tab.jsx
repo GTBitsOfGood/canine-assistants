61b18e036156fc36586e1fca340a14d451eccd99
DanielChee <Danielpchee@gmail.com>
2024-02-28 00:12:36 -0500

    red dot

--- src/components/Tab/Tab.jsx
+++ src/components/Tab/Tab.jsx
@@ -6,17 +6,18 @@ import stringUtils from "@/utils/stringutils";
  * @param {{ activeTab: string, label: string, onTabClick: (e: React.MouseEvent<HTMLButtonElement>) => void}}
  * @returns
  */
-export default function Tab({ activeTab, label, onTabClick }) {
+export default function Tab({ activeTab, label, alertIcon, onTabClick }) {
   return (
     <li
       className={`cursor-pointer ${
         activeTab.toLowerCase() === label.toLowerCase()
           ? "border-ca-green"
           : "border-stone-50 hover:border-secondary-gray"
-      } text-xl font-bold px-7 sm:px-10 border-b-4 text-primary-text hover:bg-gray-100 pt-1`}
+      } text-xl font-bold px-7 sm:px-10 border-b-4 text-primary-text hover:bg-gray-100 pt-1 flex flex-row`}
       onClick={onTabClick}
     >
-      {stringUtils.upperFirstLetter(label)}
+      <div className="mx-1 text-red-600">{!alertIcon ? "•" : ""}</div>
+      <div>{stringUtils.upperFirstLetter(label)}</div>
     </li>
   );
 }
